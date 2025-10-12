720c223294d790de84b56d50dfb74c30697eec90
metapage CI Robot <260221+dionjwa@users.noreply.github.com>
2025-10-11 21:54:44 -0700

    CORS

--- server.ts
+++ server.ts
@@ -5,9 +5,31 @@ const STATIC_FILES_ROOT = "./dist";
 
 // Start the Deno server
 Deno.serve((req) => {
+  // Handle CORS preflight requests
+  if (req.method === 'OPTIONS') {
+    return new Response(null, {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+        'Access-Control-Max-Age': '86400',
+      },
+    });
+  }
+
   // Serve files from the specified root directory
   // The 'urlRoot' option can be used to specify a different path prefix in the URL
-  return serveDir(req, { fsRoot: STATIC_FILES_ROOT, urlRoot: "" }); 
+  const response = serveDir(req, { fsRoot: STATIC_FILES_ROOT, urlRoot: "" });
+  
+  // Add CORS headers to all responses
+  if (response instanceof Response) {
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+  }
+  
+  return response;
 }, { port: 8000 }); // Listen on port 8000
 
 console.log(`Static file server running on http://localhost:8000 serving from ${STATIC_FILES_ROOT}`);
